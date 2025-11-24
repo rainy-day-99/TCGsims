e73ebd6555b50c0e4856ff43271e68c57821d78c
rainy-day-99 <angusmbryant@gmail.com>
2025-11-24 09:26:08 -0600

    Exports all results, sorted, as an Excel spreadsheet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Arkhite.research import game
+from Tests.trigger import game
 import numpy as np
 import pandas as pd
 from time import time
@@ -7,29 +7,30 @@ from Algorithms.montecarlo import local_search
 from Algorithms.bruteforce import broad_search
 from os import get_terminal_size
 
-def single_test(game = game, num_sims = 100000):
+
+def single_test(game = game, num_sims = 200000):
       initial_deck = game.CreateInitialDeck()
       game.RunGames(initial_deck, num_sims)
       return {initial_deck.key: initial_deck}
 
 search_choice = {
-      'local-search': local_search,
-      'brute-force': broad_search,
+      'local-search': local_search, 
+      'broad-search': broad_search,
       'single-test': single_test
 }
 
 start = time()
-decks = search_choice['local-search'](game)
+decks = search_choice['broad-search'](game)
 duration = str(timedelta(seconds = round(time() - start)))
 
-column_names = game.variables + ['Mean', 'Score', 'n']
+column_names = game.variables + ['Score', 'n', 'Mean']
 table = pd.DataFrame(columns=column_names)
 total_games_played = 0
 for i, deck in enumerate(decks.values()):
       mu = np.around(np.mean(deck.results, axis=0), 4)
-      score = game.Score(deck, statistic = 'mean')
+      score = np.around(game.Score(deck, statistic='mean'), 4)
       n = deck.games_played
-      table.loc[i] = [deck.recipe[card] for card in game.variables] + [mu, score, n]
+      table.loc[i] = [deck.recipe[card] for card in game.variables] + [score, n, mu]
       total_games_played += deck.games_played
 
 table.sort_values("Score", ascending=False, inplace=True, ignore_index=True)
@@ -38,5 +39,14 @@ top10 = table[:10]
 print("".center(get_terminal_size()[0], '-'))
 print("   Final results   ".center(get_terminal_size()[0], ' '))
 print("".center(get_terminal_size()[0], '-'))
-print(table[:10])
+print(top10)
 print(f"\n{total_games_played} total simulations, finished in {duration}")
+
+mean_as_list = pd.DataFrame(table["Mean"].to_list())
+table.drop("Mean", axis=1, inplace=True)
+output = table.join(mean_as_list)
+
+with pd.ExcelWriter("ExcelOutputs/triggers.xlsx", engine="openpyxl", 
+                    mode="a", if_sheet_exists="replace"
+                    ) as w:
+      output.to_excel(w, sheet_name="No triggers, Twin Drive")
