3be9d01d8e16243db67fb6e8be06d65e1a06a972
rainy-day-99 <angusmbryant@gmail.com>
2026-02-17 09:43:57 -0600

    changed index to id

--- helper.py
+++ helper.py
@@ -13,9 +13,9 @@ def draw(hand: list[int], cards: list[Card], deck: list[int], add: bool=True):
         weights=deck, 
         k=1)
     draw = top_of_deck[0]
-    deck[draw.index] -= 1
+    deck[draw.id] -= 1
     if add == True:
-        hand[draw.index] += 1
+        hand[draw.id] += 1
     return hand, deck, draw
 
 def debugprint(msg: str, debug_mode):
